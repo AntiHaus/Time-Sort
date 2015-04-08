782dec0579375cccf931d04f8219ec8c8e9698a2
AntiHaus <mr.barsby.paypal@isbjorn.org>
2015-04-07 22:22:02 -0400

    Update main.swift

--- TimeSort/main.swift
+++ TimeSort/main.swift
@@ -4,9 +4,9 @@ let arr = [1,2,4,53,553,12,1,123,42,12,45,12,5,12,243,12,2,4,5,6,3,3,5,6,3,3,2,2
 
 for i in arr {
     
-    var anotherFloat: Float = Float(i) * 0.001
+    var floatIndex: Float = Float(i) * 0.001
     
-    let time = dispatch_time(DISPATCH_TIME_NOW, Int64(anotherFloat * Float(NSEC_PER_SEC)))
+    let time = dispatch_time(DISPATCH_TIME_NOW, Int64(floatIndex * Float(NSEC_PER_SEC)))
     
     dispatch_after(time, dispatch_get_main_queue()) {
         println(i)
