e6bc217d6988549bb834c9308f2a5728c47fac08
AntiHaus <contact@antihaus.com>
2015-04-07 22:14:54 -0400

    Initial Commit

---
+++ TimeSort/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TimeSort
+//
+//  Created by AntiHaus on 07/04/2015.
+//  Copyright (c) 2015 AntiHaus. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
